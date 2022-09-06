af717b54d4773979064ce8cf7e350e9cc5204577
khurshidaalam <setukhurshidaalom@gmail.com>
2022-09-06 17:34:05 +0600

    navbar dropdown add

--- src/components/layout/MainPageLayout.js
+++ src/components/layout/MainPageLayout.js
@@ -20,7 +20,7 @@ const MainPageLayout = () => {
                 }
 
 
-                <button className="minimizebtn" onClick={() => { setMinimize(!minimize) }}>
+                <button className="minimizebtn" onClick={() => { setMinimize(!minimize) }} style={{transform: minimize && `rotate(90deg)`, transition: minimize ? `all 0.3s` : `all 0.3s`}}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8" fill="none">
                         <path d="M1 1L6.29289 6.29289C6.68342 6.68342 7.31658 6.68342 7.70711 6.29289L13 1" stroke="#7D7D7E" stroke-width="1.5" stroke-linecap="round" />
                     </svg>
